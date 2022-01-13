9b946837340488097f642a5aa306d378fcb2d1f4
Aaron Harris <aaronsharris@gmail.com>
2022-01-13 15:14:54 -0600

    added classNames for Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
   return (
-    <div>
-      Form
+    <div className="form">
+      <div className="progress-bar"></div>
+      <div className="form-container">
+        <div className="header"></div>
+        <div className="body"></div>
+        <div className="footer">
+        <button>prev</button></div>
+        <button>next</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
