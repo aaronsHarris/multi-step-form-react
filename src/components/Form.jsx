a019cad33d3e2ef4837dbf423b5c99495d89ebd2
Aaron Harris <aaronsharris@gmail.com>
2022-01-13 15:35:02 -0600

    added page conditional statement in Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import AdditionalInfo from "./AdditionalInfo";
+import PersonalInfo from "./PersonalInfo";
+import SignUpInfo from "./SignUpInfo";
 
 function Form() {
   const [page, setPage] = useState(0)
 
   const FormTitles = ['Sign Up', 'Personal Info', 'Additional Info']
+
+  const pageDisplay = () => {
+    if (page === 0) {
+      return <SignUpInfo />
+    } else if (page === 1) {
+      return <PersonalInfo />
+    } else {
+      return <AdditionalInfo />
+    }
+  }
   return (
     <div className="form">
       <div className="progress-bar"></div>
@@ -11,7 +24,7 @@ function Form() {
         <div className="header">
           <h1>{FormTitles[page]}</h1>
         </div>
-        <div className="body"></div>
+        <div className="body">{pageDisplay()}</div>
         <div className="footer">
           <button
             disabled={page == 0}
